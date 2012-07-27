597595637d6abe4b53240ff2512ff33940b2d8c4
Josh Nussbaum <joshnuss@gmail.com>
2012-07-26 22:16:40 -0400

    added ability to read and write gpio pins

--- lib/bcm2835/gpio.rb
+++ lib/bcm2835/gpio.rb
@@ -12,9 +12,31 @@ module Bcm2835
 		end
 
 		def input(pin)
+			Native.bcm2835_gpio_fsel(pin, Native::GPIO_FSEL_INPT)
 		end
 
 		def output(pin)
+			Native.bcm2835_gpio_fsel(pin, Native::GPIO_FSEL_OUTP)
 		end
+
+		def set(pin)
+			Native.bcm2835_gpio_set(pin)
+		end
+
+		def clear(pin)
+			Native.bcm2835_gpio_clr(pin)
+		end
+
+		def read(pin)
+			Native.bcm2835_gpio_lev(pin) == Native::HIGH
+		end
+
+		alias :[] :read
+
+		def []=(pin, value)
+			value ? set(pin) : clear(pin)
+		end
+
+		module_function :direction, :input, :output, :set, :clear, :read, :[], :[]=
 	end
 end
--- lib/bcm2835/native.rb
+++ lib/bcm2835/native.rb
@@ -4,8 +4,25 @@ module Bcm2835
 	module Native
 		extend FFI::Library
 		ffi_lib '/usr/local/lib/libbcm2835.so'
+
+    GPIO_FSEL_INPT = 0b000
+    GPIO_FSEL_OUTP = 0b001
+    GPIO_FSEL_ALT0 = 0b100
+    GPIO_FSEL_ALT1 = 0b101
+    GPIO_FSEL_ALT2 = 0b110
+    GPIO_FSEL_ALT3 = 0b111
+    GPIO_FSEL_ALT4 = 0b011
+    GPIO_FSEL_ALT5 = 0b010
+    GPIO_FSEL_MASK = 0b111
+
+		LOW  = 0
+		HIGH = 1
 		
 		attach_function :bcm2835_init, [], :int
 		attach_function :bcm2835_close, [], :int
+		attach_function :bcm2835_gpio_fsel, [:int, :int], :void
+		attach_function :bcm2835_gpio_set, [:int], :void
+		attach_function :bcm2835_gpio_clr, [:int], :void
+		attach_function :bcm2835_gpio_lev, [:int], :int
 	end
 end
